a6d926cb15372483370b0ccbe39fbb0c50e32a97
Moo <mutopia82@gmail.com>
2024-12-09 21:13:06 +0900

    Initial commit

---
+++ bewavoca-iOS/bewavoca-iOS/Sources/App/bewavocaApp.swift
@@ -0,0 +1,17 @@
+//
+//  bewavoca_iOSApp.swift
+//  bewavoca-iOS
+//
+//  Created by Muchan Kim on 12/9/24.
+//
+
+import SwiftUI
+
+@main
+struct bewavocaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            
+        }
+    }
+}
